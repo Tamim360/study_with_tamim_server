c41cb9ece584848b788d7d014f52c87b4e37d007
Tamimul Islam <hmtamim24@gmail.com>
2022-11-08 20:10:36 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // imports
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config();
 
@@ -13,6 +14,26 @@ app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send(`server is running on port ${port}`);
+})
+
+// database connection
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qbqevch.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        
+    }
+    finally {
+        
+    }
+}
+
+// services api
+app.get('/services', (req, res) => {
+
 })
 
 app.listen(port, () => {
